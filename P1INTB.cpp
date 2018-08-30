396748e61d0f0c43521a5fe0b24f1ead6dbbb1a2
amay723 <31995344+amay723@users.noreply.github.com>
2018-08-30 14:44:13 -0700

    Add files via upload

--- P1INTB.cpp
+++ P1INTB.cpp
@@ -8,14 +8,15 @@ using namespace std;
 void printAry();
 void printGrid(int *board[9]) {for( int i = 0; i < 9; i++) {for( int j = 0; j < 9; j++) {cout << board[i][j] << ' '; }  cout << endl;} }
 bool solve ( int *board[9] );
-void checkCell(int *board[9], int idx);
+void checkCell(int *board[9], int idx, int *available);
 int getStart ();
 bool done ();
-int available[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
-void printA() { for (int i = 0;i<9;i++){cout<< available[i];}cout << endl;}
-void resetA() { for (int i = 0; i<9; i++){available[i] = 1;}}
+//int available[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+void printA(int *available) { for (int i = 0;i<9;i++){cout<< available[i];}cout << endl;}
+//void resetA(int available[9]) { for (int i = 0; i<9; i++){available[i] = 1;}}
 int *construct (string input, int *board[9], int lineo);
 bool ary[81] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int count = 0;
 int main (int argc, char * argv [])
 {
 	int *board[9];
@@ -49,8 +50,12 @@ int main (int argc, char * argv [])
     		}	
     	cout << endl;
   	}
-
+	
+	//int available[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
 	solve(board);
+	cout << "----------------------" << endl;  
+	printGrid(board);
+	cout << "This took " << count << "tries to complete\n";
 
         return 0;
 }
@@ -70,46 +75,40 @@ int *construct (string input, int *board[9], int lineno) {
 
 bool solve(int *board[9] ) {
 	//cout << "CALLING DONE" << endl;
-	printGrid(board);
-	printAry();
-	cout << "----------------------" << endl;
+  //printGrid(board);
+  //	printAry();
+  //cout << "----------------------" << endl;
 	if ( done() ) {return true;}
 	//cout << "DONE CALLED" << endl;
 	int start = getStart();
 	int row = start / 9;
         int col = start % 9;
 	//printA();
-	resetA();
-	//int available[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
-	checkCell(board, start);
-	printA();
+	//resetA();
+
+	int available[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+	
+	checkCell(board, start, available);
+	//	printA(available);
+	//cout << available << endl;
 	for (int i = 0;i<9;i++ ) {
 		if ( available[i] != 0) {
+		  count++;
 			//cout << "INDEX OF AVAILABLE" << i
 			board[row][col] =i + 1;
-			cout << "VALUE TO PLACE IN BOARD" << i + 1 << endl;
+			//cout << "VALUE TO PLACE IN BOARD: " << i + 1 << endl;
 			available[i] = 0;
 			ary[col+(row*9)] = 1;
 			if (solve(board)) {
 				return true;
 			}
-			resetA();
+			//resetA();
+			//available = {1, 1, 1, 1, 1, 1, 1, 1, 1};
 			board[row][col] = 0;
 			ary[col+(row*9)] = 0;
 		}
 	}
-	// check to see if every node was visited.
-	// If not start with first open space.
-	// Check the row collumn and box for available #'s
-	// For each value available check all posibilities.
-	/*
-	for ( int i = 0; i<9;i++){
-		for (int j=0;j<9;j++){
-			cout << board[i][j] << endl;
-		}
-	}
-	*/
-	cout << "ERROR FOUND\n";
+	//printA(available);
 	return false;
 }
 bool done() {
@@ -129,7 +128,7 @@ int getStart() {
 	}
 	
 }
-void checkCell(int *board[9], int idx) {
+void checkCell(int *board[9], int idx, int *available) {
 	//cout << "START INDEX: " << idx << endl;
   	int row = idx / 9;
   	int col = idx % 9;
@@ -144,9 +143,7 @@ void checkCell(int *board[9], int idx) {
   	}
 	
 	//return available;
-	cout << "ROWS CHECKED" << endl;
-	cout << "AVAIlABLE LIST: ";
-	printA();
+	//printA(available);
 	// Check cols
   	for( int j = 0; j < 9; j++) {
     		if( board[j][col] != 0 ) {
@@ -154,9 +151,8 @@ void checkCell(int *board[9], int idx) {
 			available[ temp - 1 ] = 0;
 		}
   	}	
-	cout << "COLS CHECKED" << endl;
-	cout << "AVAIlABLE LIST: ";
-	printA();
+
+	//printA(available);
 	//return available;
   	int startSectorCol = (col / 3) * 3;
   	int startSectorRow = (row / 3) * 3;
@@ -165,7 +161,7 @@ void checkCell(int *board[9], int idx) {
   	for( int i = startSectorRow; i < startSectorRow + 3; i++) {
     		for( int j = startSectorCol; j < startSectorCol + 3; j++) {
       			if( board[i][j] != 0 ) {
-			  cout << "NUMBER FOUND IN SECTOR: " << board[i][j] << endl;
+			  //cout << "NUMBER FOUND IN SECTOR: " << board[i][j] << endl;
 				int temp = board[i][j];
 				//cout << "VALUE IN BOARD: " << temp <<"| VALUES IN i & j: " << i << " " << j << endl;
 				available[temp - 1] = 0;
@@ -173,10 +169,11 @@ void checkCell(int *board[9], int idx) {
     		}
   	}
 
+	/*
 	cout << "SECTORS CHECKED" << endl;
 	cout << "AVAIlABLE LIST: ";
-	printA();
-	
+	printA(available);
+	*/
 }
 void printAry() { for (int i = 0;i<81;i++){cout<< ary[i];}cout << endl;}
 
